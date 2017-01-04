dbfef5c98220188fa72f7a93b0e9fc98ee1992ff
jgatonye <gatonyejmaina@gmail.com>
2017-01-04 17:41:40 +0300

    more changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 
 
@@ -24,4 +25,10 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
         .bundle()
         .pipe(source('app.js'))
         .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function() {
+    return gulp.src("./build/js/app.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("./build/js"));
 });
