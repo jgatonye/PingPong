24795c7ebd555f95c41f1cbddc8852f1ac0f1e98
jgatonye <gatonyejmaina@gmail.com>
2017-01-05 11:12:12 +0300

    gulp bower

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,19 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var buildProduction = utilities.env.production;
-var lib = require('bower-files')();
+var lib = require('bower-files')({
+    "overrides": {
+        "bootstrap": {
+            "main": [
+                "less/bootstrap.less",
+                "dist/css/bootstrap.css",
+                "dist/js/bootstrap.js"
+            ]
+        }
+    }
+});
+var browserSync = require('browser-sync').create();
+var jshint = require('gulp-jshint');
 
 
 
@@ -54,9 +66,33 @@ gulp.task("build", ['clean'], function() {
 });
 
 
-gulp.task('bowerJS', function () {
-  return gulp.src(lib.ext('js').files)
-    .pipe(concat('vendor.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
+gulp.task('bowerJS', function() {
+    return gulp.src(lib.ext('js').files)
+        .pipe(concat('vendor.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/js'));
+});
+
+
+gulp.task('jshint', function() {
+    return gulp.src(['js/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('bowerCSS', function() {
+    return gulp.src(lib.ext('css').files)
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task('build', ['clean'], function() {
+    if (buildProduction) {
+        gulp.start('minifyScripts');
+    } else {
+        gulp.start('jsBrowserify');
+    }
+    gulp.start('bower');
 });
